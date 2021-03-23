591c2ddb2ba7ded1366fce278d3e020e718f8c29
Anirudh Sridhar <61239478+Anirudh1905@users.noreply.github.com>
2021-03-24 00:49:24 +0530

    Create interleave_queue.cpp

---
+++ interleave_queue.cpp
@@ -0,0 +1,58 @@
+// C++ program to interleave the first half of the queue
+// with the second half
+#include <bits/stdc++.h>
+using namespace std;
+
+// Function to interleave the queue
+void interLeaveQueue(queue<int>& q)
+{
+	stack<int>st;int n=q.size();
+	while(!q.empty())
+	{
+	    st.push(q.front());
+	    q.pop(); 
+	}
+	while(!st.empty())
+	{
+	    q.push(st.top());
+	    st.pop();
+	}
+	while(!q.empty())
+	{
+	    st.push(q.front());
+	    q.pop(); 
+	}
+	while(q.size()!=st.size())
+	{
+	    q.push(st.top());
+	    st.pop();
+	}
+	while(!st.empty())
+	{
+	    q.push(q.front());q.pop();
+	    q.push(st.top());st.pop();
+	}
+}
+
+// Driver program to test above function
+int main()
+{
+	queue<int> q;
+	q.push(11);
+	q.push(12);
+	q.push(13);
+	q.push(14);
+	q.push(15);
+	q.push(16);
+	q.push(17);
+	q.push(18);
+	q.push(19);
+	q.push(20);
+	interLeaveQueue(q);
+	int length = q.size();
+	for (int i = 0; i < length; i++) {
+		cout << q.front() << " ";
+		q.pop();
+	}
+	return 0;
+}
