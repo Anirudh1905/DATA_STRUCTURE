d2159a188646dd6c5585e2d6f51e33b15b7b62b9
Anirudh Sridhar <61239478+Anirudh1905@users.noreply.github.com>
2021-04-24 15:47:58 +0530

    Create Min edge rev.cpp

---
+++ Min edge rev.cpp
@@ -0,0 +1,30 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+    int v,e,dest;cin>>v>>e>>dest;
+    vector<pair<int,int>>g[v];
+    vector<int>d(v,INT_MAX);d[0]=0;
+    for(int i=0;i<v;i++)
+    {
+        int x,y;
+        cin>>x>>y;
+        g[x].push_back({y,0});
+        g[y].push_back({x,1});
+    }
+    for(int i=0;i<v;i++)
+    {   
+        if(d[i]!=INT_MAX)
+        {
+            for(auto x: g[i])
+            {
+                int w=x.second,u=d[i],v=d[x.first];
+                if(u+w<v)
+                    d[x.first]=u+w;
+            }
+        }
+    }
+    cout<<d[dest];
+    return 0;
+}
