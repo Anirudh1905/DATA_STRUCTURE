d45c8d2428aab1702ed63da23a0c61799f4c2bed
Anirudh Sridhar <61239478+Anirudh1905@users.noreply.github.com>
2021-05-21 19:10:07 +0530

    Update largest submatrix with sum 0.cpp

--- largest submatrix with sum 0.cpp
+++ largest submatrix with sum 0.cpp
@@ -2,8 +2,9 @@
 // sub-matrix whose sum is 0
 #include <bits/stdc++.h>
 using namespace std;
+#define MAX_ROW 10
+#define MAX_COL 10
 
-const int MAX = 100;
 pair<int,pair<int,int>> subsum(int arr[],int n)
 {
     unordered_map<int,int>mp;int a,b,s=0,ans=INT_MIN;
@@ -22,18 +23,23 @@ pair<int,pair<int,int>> subsum(int arr[],int n)
         {
             if(ans<i-mp[s])
             {
-                ans=i-mp[s]+1;
-                a=mp[s];b=i;
+                ans=i-mp[s];
+                a=mp[s]+1;b=i;
             }
         }
-        mp[s]=i;
+        else
+            mp[s]=i;
     }
     return {ans,{a,b}};
 }
 
-void sumZeroMatrix(int m[][MAX], int r, int c)
+void sumZeroMatrix(int m[][MAX_COL], int r, int c)
 {
     int ans=INT_MIN,fup=0,fdown=0,fleft=0,fright=0;
+    for(int i=0;i<r;i++)
+        for(int j=0;j<c;j++)
+            if(m[i][j]==0)
+                m[i][j]=-1;
 	for(int i=0;i<r;i++)
 	{
 	    int a[c];memset(a,0,sizeof(a));
@@ -44,31 +50,26 @@ void sumZeroMatrix(int m[][MAX], int r, int c)
 	           a[k]+=m[j][k]; 
 	        }
 	        auto x=subsum(a,c);
-	        //cout<<(j-i+1)*x.first<<endl;
-	        if((j-i+1)*x.first>ans)
+	        if((j-i+1)*(x.first)>ans)
 	        {
 	            fup=i;fdown=j;fleft=x.second.first;fright=x.second.second;
-	            ans=(j-i+1)*x.first;
+	            ans=(j-i+1)*(x.first);
 	        }
 	    }
 	}
 	cout<<ans<<endl;
-	// Print final values
-	for (int j = fup; j <= fdown; j++)
-	{
-		for (int i = fleft; i <= fright; i++)
-			cout << m[j][i] << " ";
-		cout << endl;
-	}
+	cout<<"("<<fup<<","<<fleft<<") ("<<fdown<<","<<fright<<")";
 }
 
 // Driver program to test above functions
+// for finding max sum comment that 0 conversion to -1
 int main()
 {
-	int a[][MAX] = { { 9, 7, 16, 5 }, { 1, -6, -7, 3 },
-					{ 1, 8, 7, 9 }, { 7, -2, 0, 10 } };
-
-	int row = 4, col = 4;
+	int a[MAX_ROW][MAX_COL] = { {0, 0, 1, 1},
+                                    {0, 1, 1, 0},
+                                    {1, 1, 1, 0},
+                                  {1, 0, 0, 1} };    
+    int row = 4, col = 4;
 	sumZeroMatrix(a, row, col);
 	return 0;
 }
